06116aca78d3df2f526161d74136711c282b4f84
Maximanu <151451667+MaximanuMC@users.noreply.github.com>
2024-11-02 17:31:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navList = document.querySelector(".nav-list");
+    const modal = document.getElementById("modal");
+    const closeButton = document.querySelector(".close-button");
+    const animatedButton = document.querySelector(".animated-button");
+
+    mobileMenu.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+
+    animatedButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
